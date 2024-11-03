e3ada071ba42aec21fa637e71decfefee2dfa9f5
nodracPM <nodraccpm14@gmail.com>
2024-11-03 00:43:12 -0600

    agrega el método de inserción en el árbol rojo-negro

--- arbolRedBlack/Pruebas.java
+++ arbolRedBlack/Pruebas.java
@@ -4,5 +4,14 @@ public class Pruebas {
         RedBlackTree tree = new RedBlackTree(root);
     
         tree.insert(3);
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(9);
+        
+        System.out.println("BFS on the red black tree:");
+        tree.showTree();
     }
 }
